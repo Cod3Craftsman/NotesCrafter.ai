421b530869158d2fb567a77097fe831ca1d178ad
Ankit Gupta <ag5756042@gmail.com>
2026-04-03 16:43:01 +0530

    feat: implement Google authentication with user creation and token management

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -2,15 +2,20 @@ import { signInWithPopup } from 'firebase/auth';
 import { motion } from 'framer-motion'
 import { FcGoogle } from "react-icons/fc";
 import { auth, provider } from '../utils/firebase.js';
-
+import axios from 'axios';
+import { serverUrl } from '../App.jsx';
 function Auth() {
 
   const handleGoogleAuth = async () => {
     try {
       const response = await signInWithPopup(auth, provider)
-      console.log(response)
+      const User = response.user
+      const name = User.displayName
+      const email = User.email
+      const result = await axios.post(`${serverUrl}/api/auth/google`, { name, email }, { withCredentials: true })
+      console.log(result.data)
     } catch (error) {
-      console.log("Error in handleGoogleAuth",error)
+      console.log("Error in handleGoogleAuth", error)
     }
   }
 
--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./utils/db.js";
+import authRouter from "./routes/auth.route.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.json({ message: "ExamNotes AI Backend Running 🚀" });
 });
 
+app.use("/api/auth", authRouter);
+
 app.listen(PORT, () => {
   console.log(`✅Server running on port ${PORT}`);
-  connectDb()
+  connectDb();
 });
