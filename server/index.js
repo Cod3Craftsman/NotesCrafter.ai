58c2a88f08ffdd55e1d075f9438f5a72b237190f
Ankit Gupta <ag5756042@gmail.com>
2026-05-14 23:58:13 +0530

    feat: implement note generation feature with AI integration and user credit management

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
+import notesRouter from "./routes/generate.route.js";
 dotenv.config();
 
 const app = express();
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/notes", notesRouter);
 
 app.listen(PORT, () => {
   console.log(`✅Server running on port ${PORT}`);
---
+++ server/models/notes.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+const notesSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserModel",
+      required: true,
+    },
+
+    topic: {
+      type: String,
+      required: true,
+    },
+
+    classLevel: String,
+    examType: String,
+
+    revisionMode: {
+      type: Boolean,
+      default: false,
+    },
+
+    includeDiagram: {
+      type: Boolean,
+      default: false,
+    },
+
+    includeChart: {
+      type: Boolean,
+      default: false,
+    },
+
+    content: {
+      type: mongoose.Schema.Types.Mixed, // AI response (string / JSON)
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Notes = mongoose.model("Notes", notesSchema);
+export default Notes;
